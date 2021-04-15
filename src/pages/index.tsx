ef0405700e74db235a04ba30e56d7b0b7f050a5b
Caio Vinicius <caiochagas77@hotmail.com>
2021-04-15 12:51:38 -0300

    Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import styles from'../styles/home.module.scss';
 import Head from 'next/head'
 
 export default function Home() {
@@ -8,7 +7,7 @@ export default function Home() {
       <title>Inicio | ig.news</title>  
     </Head>
 
-    <h1 className={styles.title}>É isso ai world</h1>
+    <h1>Pegue, mexa e bata</h1>
     </>
    )
 }
