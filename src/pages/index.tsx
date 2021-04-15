16542338a1c7723b3cb758c8924fa486c8092b01
Caio Vinicius <caiochagas77@hotmail.com>
2021-04-15 08:34:15 -0300

    uiui

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import styles from'../styles/home.module.scss';
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Inicio | ig.news</title>  
+    </Head>
+
+    <h1 className={styles.title}>É isso ai world</h1>
+    </>
+   )
+}
